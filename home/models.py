a773ecfda86a53b9c23c0fb911d20e1089db4ca5
recorder99 <30973326+recoder99@users.noreply.github.com>
2023-06-06 16:22:59 +0800

    model draft

--- home/models.py
+++ home/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Users(models.Model):
+    user_id = models.IntegerField()
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.CharField(max_length=240)
+
+class Product(models.Model):
+    product_id = models.IntegerField()
+    product_name = models.CharField(max_length=600)
+    description = models.CharField()
+    price = models.FloatField()
+
+class Orders(models.Model):
+    order_id = models.IntegerField()
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
