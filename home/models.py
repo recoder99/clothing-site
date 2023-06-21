7f77ecdc7df4aca15ec36d5db504ddb6430f7e07
recorder99 <30973326+recoder99@users.noreply.github.com>
2023-06-21 16:46:07 +0800

    fix web not running

--- home/models.py
+++ home/models.py
@@ -1,19 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Users(models.Model):
-    user_id = models.IntegerField()
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    address = models.CharField(max_length=240)
+class Address(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    Address = models.CharField(max_length=600)
 
 class Product(models.Model):
-    product_id = models.IntegerField()
     product_name = models.CharField(max_length=600)
-    description = models.CharField()
+    description = models.CharField(max_length=69000)
     price = models.FloatField()
 
 class Orders(models.Model):
-    order_id = models.IntegerField()
     product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    delivered = models.BooleanField()
+
+class Cart:
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    ordered = models.BooleanField()
+
