8910adc666f058d0a4973e3153bc53ec9eef216e
Nicko <nicktacku777@gmail.com>
2023-06-21 17:20:50 +0800

    insert tite

--- home/models.py
+++ home/models.py
@@ -2,15 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+# tite
+
 
 class Address(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     Address = models.CharField(max_length=600)
 
+
 class Product(models.Model):
     product_name = models.CharField(max_length=600)
     description = models.CharField(max_length=69000)
     price = models.FloatField()
+
 
 class Orders(models.Model):
     product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
@@ -18,8 +22,8 @@ class Orders(models.Model):
     quantity = models.IntegerField()
     delivered = models.BooleanField()
 
+
 class Cart:
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
     ordered = models.BooleanField()
-
