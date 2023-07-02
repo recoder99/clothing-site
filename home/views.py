b96c9124359ccc44b9f0bc2906cadbd891150058
recorder99 <30973326+recoder99@users.noreply.github.com>
2023-07-03 03:26:32 +0800

    *templates are now in static. *assets are also moved to static folder. *href links are updated with its appropriate url paths

--- home/views.py
+++ home/views.py
@@ -5,3 +5,12 @@ from django.http import HttpResponse
 # Create your views here.
 def homepage(request):
     return render(request, "home/index.html")
+
+def faq(request):
+    return render(request, "home/faq.html")
+
+def login(request):
+    return render(request, "home/login.html")
+
+def signup(request):
+    return render(request, "home/signup.html")
