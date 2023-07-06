6363b0189e8bedb3a2795f243905de993b384a32
recorder99 <30973326+recoder99@users.noreply.github.com>
2023-07-07 03:27:53 +0800

    product adding functionality added
    *Image upload functionality implemented
    *Can be found on admin page
    
    Admin credentials:
    
    username: admin
    password: katomegumi

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product
 
 # Register your models here.
+class ProductsAdmin(admin.ModelAdmin):
+    fields = ['product_name', 'description', 'price', 'product_image']
+
+admin.site.register(Product, ProductsAdmin)
