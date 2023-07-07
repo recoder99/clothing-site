774a5c626d3110365ef23284ff4ed2687ebdcb45
Nicko <nicktacku777@gmail.com>
2023-07-07 16:49:07 +0800

    mock product page design

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.homepage, name='home-page'),
     path('faq', views.faq, name='faq'),
     path('login', views.login, name='login'),
-    path('signup', views.signup, name='signup')
+    path('signup', views.signup, name='signup'),
+    path('product_page', views.product_page, name='product-page')
 ]
--- home/views.py
+++ home/views.py
@@ -8,29 +8,36 @@ from django.contrib import redirects
 
 # Create your views here.
 def homepage(request):
-    
+
     products = Product.objects.all()
-    return render(request, "home/index.html", {'products': products})
+    return render(request, "home/index.html", {"products": products})
+
 
 def faq(request):
     return render(request, "home/faq.html")
 
+
 def login(request):
     return render(request, "home/login.html")
 
+
 def signup(request):
-    
+
     form = UserCreationForm()
     print("test_point_0")
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserCreationForm(request.POST)
         print("test_point_1")
         if form.is_valid():
             print("test_point_2")
             form.save()
-            
-    
+
     context = {}
 
-    context['form'] =  form
+    context["form"] = form
     return render(request, "home/signup.html", context)
+
+
+def product_page(request):
+    product = Product.objects.get(product_name="Black Skeleton")
+    return render(request, "home/product_page.html", {"product": product})
