4b362368173586b4f7350a63e7a8eb4b6ff9652a
recoder99 <roelccastro@iskolarngbayan.pup.edu.ph>
2023-07-09 01:17:08 +0800

    category model refactor (warning: not working)

--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
-from .models import Product
+from .models import *
+
+class CategoryInline(admin.TabularInline):
+    model = Category
 
 # Register your models here.
 class ProductsAdmin(admin.ModelAdmin):
     fields = ['product_name', 'description', 'price', 'product_image']
+    inlines = [CategoryInline]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ['category_name']
 
 admin.site.register(Product, ProductsAdmin)
+admin.site.register(Category, CategoryAdmin)
