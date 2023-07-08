d0d196a52f9fdf61997b6b0f6958fc7c1086cbac
Nicko <nicktacku777@gmail.com>
2023-07-09 01:38:10 +0800

    change quantity

--- home/static/quantityIncrement.js
+++
@@ -1,9 +0,0 @@
-function incrementButton(){
-    var element = document.getElementById("quantity")
-    var totalValue = element.value
-
-    ++totalValue
-
-    console.log(value)
-    document.getElementById("quantity").value = totalValue
-}
